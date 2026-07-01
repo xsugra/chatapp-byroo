b557f6d9ef7e8a18c5ee9dfa2acfd37c7ab9fa14
Samuel Šugra <xsugra@stuba.sk>
2026-07-01 18:26:33 +0200

    feat: implement message highlighting for own messages and auto-scroll in chat

--- src/ChatApp.Client/Views/MainView.xaml.cs
+++ src/ChatApp.Client/Views/MainView.xaml.cs
@@ -1,3 +1,4 @@
+using System.Collections.Specialized;
 using System.Windows;
 
 namespace ChatApp.Client.Views;
@@ -7,5 +8,19 @@ public partial class MainView : Window
     public MainView()
     {
         InitializeComponent();
+        Loaded += OnLoaded;
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        if (MessagesListBox.ItemsSource is INotifyCollectionChanged collection)
+        {
+            collection.CollectionChanged += (_, _) =>
+            {
+                if (MessagesListBox.Items.Count > 0)
+                    MessagesListBox.ScrollIntoView(
+                        MessagesListBox.Items[^1]);
+            };
+        }
     }
 }
